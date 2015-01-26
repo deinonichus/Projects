1960f8a140417f02053d263b7713ed01de8ba320
deinonichus <ds.lutz@gmail.com>
2015-01-26 21:51:49 +0100

    semantic

---
+++ semanticNetwork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semanticNetwork {
+    class Node {
+
+    }
+
+    class Link {
+        
+    }
+
+    class Search {
+        
+
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            
+
+
+        }
+    }
+}
