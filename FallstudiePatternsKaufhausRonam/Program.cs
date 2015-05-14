0bf857add9798b5e38d07df898af7a1f8f59786f
deinonichus <ds.lutz@gmail.com>
2015-05-14 12:22:53 +0200

    .

---
+++ FallstudiePatternsKaufhausRonam/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallstudiePatternsKaufhausRonam {
+    class Program {
+        static void Main(string[] args) {
+            Mediator genericMediator = new Mediator("generic");
+
+            Panel getränke = new Panel("getränke", genericMediator);
+            Panel esswaren = new Panel("esswaren", genericMediator);
+            Panel spielwaren = new Panel("spielwaren", genericMediator);
+
+            genericMediator.addPanel(getränke);
+            genericMediator.addPanel(esswaren);
+            genericMediator.addPanel(spielwaren);
+
+            genericMediator.displayMessage("neue Aktionen", getränke);
+
+            Console.ReadKey();
+        }
+
+        class Mediator {
+            string mediatorName;
+            List<Panel> panelList = new List<Panel>();
+
+            public Mediator(string mediatorName) {
+                this.mediatorName = mediatorName;
+            }
+
+            public void addPanel(Panel panel) {
+                panelList.Add(panel);
+            }
+
+            public void broadcastMessage(string message, Panel senderPanel) {
+                foreach (Panel panel in panelList) {
+                    if (senderPanel.getName() != panel.getName()) {
+                        Console.WriteLine("Sender: " + panel.getName() + ":\t" + panel.displayMessage());
+                    }
+                }
+            }
+
+            public void displayMessage(string message, Panel targetPanel) {
+                Console.WriteLine("Panel " + targetPanel.getName() + ":\t\"" + targetPanel.displayMessage() + "\"");
+            }
+        }
+
+        class Panel {
+            string panelName;
+            List<Message> messageQueue = new List<Message>();
+
+            public Panel(string panelName, Mediator mediator) {
+                this.panelName = panelName;
+            }
+
+            public string displayMessage() {
+                return message;
+            }
+
+            public string getName() {
+                return panelName;
+            }
+
+            public void addMessage(Message message) {
+                messageQueue.Add(message);
+            }
+
+            public void removeMessage(Message message) {
+                messageQueue.Remove(message);
+            }
+        }
+
+        class Message {
+            string message;
+            string title;
+            bool visible = false;
+
+            public Message(string message, string title) {
+                this.message = message;
+                this.title = title;
+            }
+
+            public void setMessage(string message) {
+                this.message = message;
+            }
+
+            public string getMessage() {
+                return message;
+            }
+
+            public void disableMessage() {
+                visible = false;
+            }
+
+            public bool isVisible() {
+                return visible;
+            }
+        }
+    }
+}
---
+++ VisitorTemplate/VisitorTemplate/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorTemplate {
+    interface ICarElementVisitor {
+        void visit(Wheel wheel);
+        void visit(Engine engine);
+        void visit(Body body);
+        void visit(Car car);
+    }
+ 
+    interface ICarElement {
+        void accept(ICarElementVisitor visitor); // CarElements have to provide accept().
+    }
+ 
+    class Wheel : ICarElement {
+        private String name;
+ 
+        public Wheel(String name) {
+            this.name = name;
+        }
+ 
+        public String getName() {
+            return this.name;
+        }
+ 
+        public void accept(ICarElementVisitor visitor) {
+            /*
+             * accept(ICarElementVisitor) in Wheel :
+             * accept(ICarElementVisitor) in ICarElement, so the call
+             * to accept is bound at run time. This can be considered
+             * the first dispatch. However, the decision to call
+             * visit(Wheel) (as opposed to visit(Engine) etc.) can be
+             * made during compile time since 'this' is known at compile
+             * time to be a Wheel. Moreover, each implementation of
+             * ICarElementVisitor : the visit(Wheel), which is
+             * another decision that is made at run time. This can be
+             * considered the second dispatch.
+             */
+            visitor.visit(this);
+        }
+    }
+ 
+    class Engine : ICarElement {
+        public void accept(ICarElementVisitor visitor) {
+            visitor.visit(this);
+        }
+    }
+ 
+    class Body : ICarElement {
+        public void accept(ICarElementVisitor visitor) {
+            visitor.visit(this);
+        }
+    }
+ 
+    class Car : ICarElement {
+        ICarElement[] elements;
+ 
+        public Car() {
+            //create new Array of elements
+            this.elements = new ICarElement[] { 
+                new Wheel("front left"),
+                new Wheel("front right"), 
+                new Wheel("back left") ,
+                new Wheel("back right"), 
+                new Body(), 
+                new Engine() 
+            };
+        }
+ 
+        public void accept(ICarElementVisitor visitor) {    
+            foreach(ICarElement elem in elements) {
+                elem.accept(visitor);
+            }
+            visitor.visit(this);    
+        }
+    }
+ 
+    //Visitor to print name of wisited part
+    class CarElementPrintVisitor : ICarElementVisitor {
+
+        public void visit(Wheel wheel) {
+            Console.WriteLine("Visiting " + wheel.getName() + " wheel");
+        }
+ 
+        public void visit(Engine engine) {
+            Console.WriteLine("Visiting engine");
+        }
+ 
+        public void visit(Body body) {
+            Console.WriteLine("Visiting body");
+        }
+ 
+        public void visit(Car car) {
+            Console.WriteLine("Visiting car");
+        }
+    }
+ 
+    //Visitor to do some action
+    class CarElementDoVisitor : ICarElementVisitor {
+        public void visit(Wheel wheel) {
+            Console.WriteLine("Kicking my " + wheel.getName() + " wheel");
+        }
+ 
+        public void visit(Engine engine) {
+            Console.WriteLine("Starting my engine");
+        }
+ 
+        public void visit(Body body) {
+            Console.WriteLine("Moving my body");
+        }
+ 
+        public void visit(Car car) {
+            Console.WriteLine("Starting my car");
+        }
+    }
+ 
+    
+    class Program {
+        static void Main(string[] args) {
+            ICarElement car = new Car();
+
+            Console.WriteLine("Start Visitor to print the parts");
+            car.accept(new CarElementPrintVisitor());
+
+            Console.WriteLine();
+            Console.WriteLine("Start Visitor to do some action");
+            car.accept(new CarElementDoVisitor());
+
+            Console.ReadLine();
+        }
+    }
+}
