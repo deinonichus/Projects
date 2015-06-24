40625bf0d318344542607d7705c9b796bb0185b9
deinonichus <ds.lutz@gmail.com>
2015-06-24 15:06:27 +0200

    '

--- Bank_test/Bank_test/UnitTest1.cs
+++ Bank_test/Bank_test/UnitTest1.cs
@@ -22,6 +22,20 @@ namespace Bank_test
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance() {
+            // arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 16.54;
+            Komp_Test.BankAccount account = new Komp_Test.BankAccount("Mr. Bryan Walton", beginningBalance);
 
+            // act
+            account.Credit(creditAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
     }
 }
