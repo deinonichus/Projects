39c320da9fd1166b509d9c4bc64947caeb0edc63
deinonichus <ds.lutz@gmail.com>
2017-11-19 21:49:06 +0100

    Assertions completed.

--- deepLearningTutorial/LogisticRegression/basicFunctions.py
+++ deepLearningTutorial/LogisticRegression/basicFunctions.py
@@ -1,21 +1,26 @@
-import math
-import time
+#import math
+#import time
 import numpy as np
 
+# TODO Write proper test cases.
 
 def sigmoid(x):
     s = 1/(1+np.exp(-x))
     return s
 
-
+x = np.array([1, 2, 3])
+sigmoid(x)
+assert str(sigmoid(x)) == str(np.array([ 0.73105858, 0.88079708, 0.95257413]))
 
 
 def sigmoid_derivative(x):
     s = sigmoid(x)
-    ds = s*(x)*(1-s)
+    ds = s*(1-s)
     return ds
 
-
+x = np.array([1, 2, 3])
+print ("sigmoid_derivative(x) = " + str(sigmoid_derivative(x)))
+assert str(sigmoid_derivative(x)) == str(np.array([ 0.19661193, 0.10499359, 0.04517666]))
 
 
 def image2vector(image):
@@ -24,6 +29,22 @@ def image2vector(image):
     return v
 
 
+# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where 3 represents the RGB values
+image = np.array([[[ 0.67826139,  0.29380381],
+                   [ 0.90714982,  0.52835647],
+                   [ 0.4215251 ,  0.45017551]],
+
+                  [[ 0.92814219,  0.96677647],
+                   [ 0.85304703,  0.52351845],
+                   [ 0.19981397,  0.27417313]],
+
+                  [[ 0.60659855,  0.00533165],
+                   [ 0.10820313,  0.49978937],
+                   [ 0.34144279,  0.94630077]]])
+
+print ("image2vector(image) = " + str(image2vector(image)))
+assert str(image2vector(image)) == str(np.array([[ 0.67826139], [ 0.29380381], [ 0.90714982], [ 0.52835647], [ 0.4215251 ], [ 0.45017551], [ 0.92814219], [ 0.96677647], [ 0.85304703], [ 0.52351845], [ 0.19981397], [ 0.27417313], [ 0.60659855], [ 0.00533165], [ 0.10820313], [ 0.49978937], [ 0.34144279], [ 0.94630077]]))
+# TODO Better assertion. Compare vectors not strings.
 
 
 def normalizeRows(x):
@@ -48,7 +69,12 @@ def normalizeRows(x):
 
     return x
 
-
+x = np.array([
+    [0, 3, 4],
+    [1, 6, 4]])
+print("normalizeRows(x) = " + str(normalizeRows(x)))
+assert str(normalizeRows(x)) == 	str(np.array([[ 0., 0.6, 0.8 ], [ 0.13736056, 0.82416338, 0.54944226]]))
+# TODO Better assertion. Compare vectors not strings.
 
 
 def softmax(x):
@@ -76,6 +102,15 @@ def softmax(x):
     ### END CODE HERE ###
 
     return s
+
+x = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(x)))
+expectedVector = np.array([[ 9.80897665e-01, 8.94462891e-04, 1.79657674e-02, 1.21052389e-04, 1.21052389e-04],
+                           [ 8.78679856e-01, 1.18916387e-01, 8.01252314e-04, 8.01252314e-04, 8.01252314e-04]])
+assert str(softmax(x)) == str(expectedVector)
+# TODO Better assertion. Compare vectors not strings.
 
 
 def L1(yhat, y):
